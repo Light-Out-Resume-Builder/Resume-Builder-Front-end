ddcbbf0f8ad6adcd0f73e1de308bb07a228e0819
Maryjanechukelu <maryjanechukelu@gmail.com>
2024-07-02 00:34:57 +0100

    components and pages created

---
+++ resume-builder/src/Pages/landing.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar";
+
+const landing = () => {
+    return (
+        <div className="" >
+             <Navbar /> 
+        </div>
+          
+        
+       
+    );
+}
+
+export default landing
